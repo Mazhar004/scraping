ad43e23c3f045eb9c2f8ce9ed5e4b3bd128e9caa
Mazhar Hossain P_Linux <princemazhar.mp@gmail.com>
2021-01-21 19:56:59 +0600

    Project Start

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# scraping
-Different scraping technolgies used to buildvarious tools
+# Scraping
+Different scraping technolgies used to build various data extractions tools
+
+## BeautifulSoup
+### Library Install
+- `pip install bs4`
+- `pip install requests`
+### Soultions
+- **Holiday calender** based on country name [Link](/beautifulsoup/Hoilday_srap.ipynb)
+
+## Scrapy
+### Solutions
+- **Amazon** best sellers books data collections
+- Basics of scrapy on **toscrape** website
+
+## Selenium
+### Library Install
+- `sudo apt-get install firefox-geckodriver`
+- `pip install bs4`
+- `pip install requests`
+### Soultions
+- Bigboss voting app [Account sign up] [Link](/selenium/BigBoss_Voting_App.ipynb)
+- Food branch details based on country name [Link](selenium/Food_branch.ipynb)
+- House rent/sale details [Link](selenium/HAR_house.ipynb)
+- A website member details extractions [Link](selenium/KPAA_member_search.ipynb)
+- Linkedin account email extraction [Login] [Link](selenium/LinkedIn_Email_Extractor.ipynb)
+
+
+
---
+++ scrapy/amazon/amazon/dataset.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+
+class Database():
+    def __init__(self, database='amazon_books.db'):
+        self.connection(database, status='connect')
+
+    def connection(self, database='amazon_books.db', status='connect'):
+        if status == 'connect':
+            self.conn = sqlite3.connect(database)
+            self.curr = self.conn.cursor()
+        elif status == 'commit':
+            self.conn.commit()
+        else:
+            self.conn.commit()
+            self.conn.close()
+
+    def create_table(self, table, field_dict, exist_ok=True):
+        field = []
+        for i, j in field_dict.items():
+            field.append('{} {}'.format(i, j))
+        field = ','.join(field)
+        response = self.execute('create table {}({})'.format(table, field))
+        if response and (exist_ok == False):
+            self.execute('drop table {}'.format(table))
+            self.execute('create table {}({})'.format(table, field))
+            print("New table is created")
+
+    def execute(self, string):
+        try:
+            self.curr.execute('''{}'''.format(string))
+        except Exception as err:
+            print(err)
+            return 1
+
+
+'''
+a = Database()
+a.create_table('quote_data', {'quote': 'text',
+                              'author': 'text', 'tag': 'text'}, exist_ok=False)
+a.execute('insert into quote_data values ("Hello1","Me1","Prince1")')
+a.connection(status=False)
+'''
